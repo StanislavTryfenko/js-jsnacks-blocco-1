5fd86af1b586517a82ca6054171eb9f496610f3d
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-26 12:30:54 +0100

    Clear code & add comments

--- snack 6/main.js
+++ snack 6/main.js
@@ -1,16 +1,12 @@
-
-
-const numberList= [];
-
+// ask how many numbers do u want to insert
 const numberOfNumbers = Number(prompt('quanti numeri vuoi inserire?'))
 
+//cycling numbers u want to cube
 for (let i = 0; i < numberOfNumbers; i++) {
-    
     
     const number = Number(prompt('inserisci un numero'))
 
     console.log(number * number * number);
-
 }
 
 
