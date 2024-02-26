bc4da9e520ba222f60f9e7c27f94fb06deaf7118
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-26 12:23:46 +0100

    Add Now u can input 5 numbers

--- snack 6/main.js
+++ snack 6/main.js
@@ -1,3 +1,15 @@
-const number = Number(prompt('inserisci un numero'))
 
-console.log(number * number * number);
+
+const numberList= [];
+
+
+for (let i = 0; i < 5; i++) {
+    
+    
+    const number = Number(prompt('inserisci un numero'))
+
+    console.log(number * number * number);
+
+}
+
+
