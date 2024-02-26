37a59a60859d2f9b29d9ceadbead1d94fb77540e
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-26 12:00:48 +0100

    clear code snack 5

--- snack 5/main.js
+++ snack 5/main.js
@@ -1,35 +1,26 @@
-const numbers = []
+const numbers = [];
 
+//using while
 let ask = 0;
-
 while(ask < 6){
-
     let input = Number(prompt("inserisci un numero"))
-
     if (input % 2 !== 0) {
-        numbers.push(input);
-        
+        numbers.push(input);  
     }
-
     ask++
 };
 
+//using for (imo best)
 /* for (let i = 0; i < 6; i++) {
-
     let input = Number(prompt("inserisci un numero"))
-
-        if (input % 2 !== 0) {
-        numbers.push(input);
-        
+    if (input % 2 !== 0) {
+    numbers.push(input);   
     }
+}; */
+
+/*  check
+console.log(numbers); */
 
 
-    
-} */
-
-
-
-
-/* const dispari = numbers.filter(number => number % 2 !== 0 ) */
-
-console.log(numbers);
+//advanced step
+//const dispari = numbers.filter(number => number % 2 !== 0 )
