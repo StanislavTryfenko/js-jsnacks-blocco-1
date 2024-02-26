04262221784381c630960aa5a93b06dac4885a69
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-02-26 11:49:21 +0100

    snack 5

---
+++ snack 5/main.js
@@ -0,0 +1,24 @@
+const numbers = []
+
+let ask = 0;
+
+while(ask < 6){
+
+    let input = Number(prompt("inserisci un numero"))
+
+    numbers.push(input);
+
+    ask++
+};
+
+/* for (let i = 0; i < 6; i++) {
+
+    let input = Number(prompt("inserisci un numero"))
+
+    numbers.push(input);
+    
+} */
+
+const dispari = numbers.filter(number => number % 2 == 0 )
+
+console.log(dispari);
